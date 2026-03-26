8a4bb42d698923eda75f3883a8a2f9614793fa2a
romatroshkin2007@gmail.com <romatroshkin2007@gmail.com>
2026-03-26 09:08:25 +0200

    no message

---
+++ MKR-TASK-1/MKR-TASK-1/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MKR_TASK_1
+{
+     class Program
+    {
+        delegate string TextOperation(string text);
+
+        static void Main()
+        {
+            string inputFile = "textPD22.txt";
+            string outputFile = "resultPD22.txt";
+            File.WriteAllText(outputFile, "");
+            ProcessFile(inputFile, outputFile, ToUpperCase);
+            ProcessFile(inputFile, outputFile, CountChars);
+            ProcessFile(inputFile, outputFile, CountWords);
+            Console.WriteLine("готово");
+        }
+        static void ProcessFile(string input, string output, TextOperation operation)
+        {
+            string[] lines= File.ReadAllLines(input);
+            foreach (string line in lines) 
+            {
+                string result = operation(line);
+                File.AppendAllText(output, result + "\n");
+            }
+
+        }
+        static string ToUpperCase(string text)
+        {
+            return text.ToUpper();
+        }
+
+        static string CountChars(string text)
+        {
+            return $"Chars: {text.Length}";
+        }
+
+        static string CountWords(string text)
+        {
+            int count = text.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).Length;
+            return $"Words: {count}";
+        }
+    }
+}
+
---
+++ MKR-TASK2/MKR-TASK2/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("MKR-TASK2")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MKR-TASK2")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("03c701a8-d009-4a52-a748-8c05086bcd26")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
